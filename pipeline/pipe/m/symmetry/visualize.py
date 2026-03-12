5b19d2329f2dc002e77203d7496f7a75ddc82631
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-03-12 15:45:44 -0600

    add category to symmetry tool

--- pipeline/pipe/m/symmetry/visualize.py
+++ pipeline/pipe/m/symmetry/visualize.py
@@ -147,6 +147,7 @@ def color_from_symmetry_error(
     name="visualize_symmetry_of_selected",
     label="Visualize Symmetry Of Selected",
     icon="symmetry.svg",
+    category="asset",
 )
 def visualize_symmetry_of_selected():
     """
